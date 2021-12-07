040a65d8be32947b6652af9b5b4be348e5c03d27
Alex <glopezalex631@gmail.com>
2021-12-07 02:32:34 -0600

    listado de usuarios

--- app/screens/Usuarios.js
+++ app/screens/Usuarios.js
@@ -1,7 +1,26 @@
 import React from "react";
+import { useEffect, useState } from 'react';
 import {View, Text} from "react-native";
+import { usuarios_todos } from "../scripts/users/users";
 
 export default function Usuarios(){
+    const [users, setUser] = useState([]);
+    const [reloadUsers, setReloadUsers] = useState(false)
+
+    useEffect(() => {
+        usuarios_todos().then(arreglo => {
+          setUser(arreglo);
+        })
+        .catch(error => {
+          console.log('Error: ',error);
+        });
+
+      setReloadUsers(false)
+    }, [reloadUsers]);
+
+    for (const user of users) {
+        console.log(user);
+    }
     return(
         <View>
             <Text>Crear usuario nuevo</Text>
